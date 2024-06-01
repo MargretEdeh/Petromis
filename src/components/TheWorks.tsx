3b1d9cc399ba98ebcae35c0ef24dd01e58f8c073
margretedeh <sagaedeh@gmail.com>
2024-06-01 12:56:50 -0400

    mobile fixed

--- src/components/TheWorks.tsx
+++ src/components/TheWorks.tsx
@@ -18,15 +18,16 @@ const TheWorks: React.FC<TheWorksProps> = ({
   bgcolor,
 }) => {
   return (
-    <div className=" md:w-[90%] h-96 shadow-2xl rounded-xl">
+    <div className=" md:w-[90%] md:h-96 shadow-2xl rounded-xl">
       <img className="object-cover w-full" src={img} />
       <div className="px-5 flex py-3  flex-col items-start font-semibold gap-3 ">
         <p className={`bg-${bgcolor} py-1 px-2 rounded-lg`}>{sub} </p>
-       <Truncate>{content} </Truncate>
+       <Truncate className='hidden md:block'>{content} </Truncate>
+       <span className='md:hidden'>{content} </span>
       </div>
        <div className=" flex p-3 text-primary justify-end ">
         {' '}
-        <button onClick={link}>Learn more </button>{' '}
+        <button className='hidden md:block' onClick={link}>Learn more </button>{' '}
       </div>
     </div>
   );
